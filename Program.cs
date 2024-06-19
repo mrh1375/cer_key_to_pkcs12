41dc89303b9110bd07c89f60109b08fe4900d00d
Mohammadreza Hoseini DELL <mrh1375@gmail.com>
2024-06-19 14:49:58 +0330

    add reverce

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Security.Cryptography;
 using System.Windows.Forms;
 using Windows.Devices.Geolocation;
 using Windows.UI.Popups;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto;
 
 
 namespace cer2pfx
@@ -14,64 +19,123 @@ namespace cer2pfx
         {
             try
             {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Certificate Files|*.crt;*.cer|All Files|*.*";
-            if (DialogResult.OK == dialog.ShowDialog())
-            {
-                string crtpath = dialog.FileName;
-                using (X509Certificate2 cert = new X509Certificate2(crtpath))
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Certificate Files|*.crt;*.cer;*.pfx|All Files|*.*";
+                if (DialogResult.OK == dialog.ShowDialog())
                 {
-                    using (RSA key = RSA.Create())
+                    string crtpath = dialog.FileName;
+                    if (crtpath.EndsWith("pfx"))
                     {
-                        dialog = new OpenFileDialog();
-                        dialog.Filter = "PrivetKey Files|*.key;*.txt|All Files|*.*";
-                        if (DialogResult.OK == dialog.ShowDialog())
+                        string certPass = "";
+                        while (string.IsNullOrWhiteSpace(certPass))
                         {
-                            string keypath = dialog.FileName;
+                            certPass = Microsoft.VisualBasic.Interaction.InputBox("پسورد را وارد کنید?", "پسورد", "");
+                        }
 
-                            string privateKeyContent = File.ReadAllText(keypath);
-                            byte[] privateKeyBytes;
-                            bool isPkcsprivateKey = privateKeyContent.Contains("BEGIN PRIVATE KEY");
-                            if (isPkcsprivateKey)
+                        // Create a collection object and populate it using the PFX file
+                        using (X509Certificate2 cert = new X509Certificate2(crtpath, certPass, X509KeyStorageFlags.Exportable))
+                        {
+                            SaveFileDialog saveFileDialog = new SaveFileDialog();
+                            saveFileDialog.Filter = "PEM Files|*.cer";
+
+
+                            if (DialogResult.OK == saveFileDialog.ShowDialog())
                             {
-                                var privateKey = privateKeyContent.Replace("-----BEGIN PRIVATE KEY-----", string.Empty).Replace("-----END PRIVATE KEY-----", string.Empty);
-                                privateKey = privateKey.Replace(Environment.NewLine, string.Empty);
-                                privateKeyBytes = Convert.FromBase64String(privateKey);
+                                string savepath = saveFileDialog.FileName;
+                                File.WriteAllText(savepath, cert.ExportCertificatePem());
                             }
-                            else
-                            {
-                                var privateKey = privateKeyContent.Replace("-----BEGIN RSA PRIVATE KEY-----", string.Empty).Replace("-----END RSA PRIVATE KEY-----", string.Empty);
-                                privateKey = privateKey.Replace(Environment.NewLine, string.Empty);
-                                privateKeyBytes = Convert.FromBase64String(privateKey);
-                            }
-                            key.ImportPkcs8PrivateKey(privateKeyBytes, out _);
 
-                            using (X509Certificate2 certWithKey = cert.CopyWithPrivateKey(key))
+                            if (cert.HasPrivateKey && cert.PrivateKey != null)
                             {
-                                string pass = "";
-                                while (string.IsNullOrWhiteSpace(pass))
-                                {
-                                    pass = Microsoft.VisualBasic.Interaction.InputBox("پسورد را وارد کنید?", "پسورد", "");
-                                }
-                                byte[] pkcs12 = certWithKey.Export(X509ContentType.Pfx, pass);
 
-                                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                                saveFileDialog.Filter = "pkcs12 Files|*.pfx";
+                                saveFileDialog = new SaveFileDialog();
+                                saveFileDialog.Filter = "Public Key File|*.pub";
+
+
                                 if (DialogResult.OK == saveFileDialog.ShowDialog())
                                 {
                                     string savepath = saveFileDialog.FileName;
-                                    File.WriteAllBytes(savepath, pkcs12);
+                                    File.WriteAllText(savepath, cert.PrivateKey.ExportSubjectPublicKeyInfoPem());
+                                }
+
+
+
+
+
+
+                                saveFileDialog = new SaveFileDialog();
+                                saveFileDialog.Filter = "Privet Key File|*.key";
+
+
+                                if (DialogResult.OK == saveFileDialog.ShowDialog())
+                                {
+                                    string savepath = saveFileDialog.FileName;
+
+                                    File.WriteAllText(savepath, cert.PrivateKey.ExportPkcs8PrivateKeyPem());
+                                }
+                            }
+
+
+
+                        }
+                    }
+                    else
+                    {
+
+                        using (X509Certificate2 cert = new X509Certificate2(crtpath))
+                        {
+                            using (RSA key = RSA.Create())
+                            {
+                                dialog = new OpenFileDialog();
+                                dialog.Filter = "PrivetKey Files|*.key;*.txt|All Files|*.*";
+                                if (DialogResult.OK == dialog.ShowDialog())
+                                {
+                                    string keypath = dialog.FileName;
+
+                                    string privateKeyContent = File.ReadAllText(keypath);
+                                    byte[] privateKeyBytes;
+                                    bool isPkcsprivateKey = privateKeyContent.Contains("BEGIN PRIVATE KEY");
+                                    if (isPkcsprivateKey)
+                                    {
+                                        var privateKey = privateKeyContent.Replace("-----BEGIN PRIVATE KEY-----", string.Empty).Replace("-----END PRIVATE KEY-----", string.Empty);
+                                        privateKey = privateKey.Replace(Environment.NewLine, string.Empty);
+                                        privateKeyBytes = Convert.FromBase64String(privateKey);
+                                    }
+                                    else
+                                    {
+                                        var privateKey = privateKeyContent.Replace("-----BEGIN RSA PRIVATE KEY-----", string.Empty).Replace("-----END RSA PRIVATE KEY-----", string.Empty);
+                                        privateKey = privateKey.Replace(Environment.NewLine, string.Empty);
+                                        privateKeyBytes = Convert.FromBase64String(privateKey);
+                                    }
+                                    key.ImportPkcs8PrivateKey(privateKeyBytes, out _);
+
+                                    using (X509Certificate2 certWithKey = cert.CopyWithPrivateKey(key))
+                                    {
+                                        string pass = "";
+                                        while (string.IsNullOrWhiteSpace(pass))
+                                        {
+                                            pass = Microsoft.VisualBasic.Interaction.InputBox("پسورد را وارد کنید?", "پسورد", "");
+                                        }
+                                        byte[] pkcs12 = certWithKey.Export(X509ContentType.Pfx, pass);
+
+                                        SaveFileDialog saveFileDialog = new SaveFileDialog();
+                                        saveFileDialog.Filter = "pkcs12 Files|*.pfx";
+                                        if (DialogResult.OK == saveFileDialog.ShowDialog())
+                                        {
+                                            string savepath = saveFileDialog.FileName;
+                                            File.WriteAllBytes(savepath, pkcs12);
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                MessageBox.Show("ورودی فابل اشتباه انتخاب شده است." + ex.Message, "خطا",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("ورودی فابل اشتباه انتخاب شده است." + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
